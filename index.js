84b54e63ee1e533da9ba7bc754de5d2e196d40bc
abhirawat623 <abhirawat623@gmail.com>
2023-08-03 21:29:01 +0530

    initial commit by me

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello, Abhi here")
+});
+
+//for heroku process.env.Port needed
+app.listen(process.env.PORT ||PORT,()=>{
+console.log("server is started now")
+});
+
