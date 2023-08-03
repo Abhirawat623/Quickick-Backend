f7ebee3946ecae33a8d8275dbf33aab2b1f28cf7
abhirawat623 <abhirawat623@gmail.com>
2023-08-04 00:15:00 +0530

    db added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+//for db
+const quizzes = require("./db/quizzes");
+
 const app = express();
 
 const PORT = 3000;
@@ -7,6 +10,10 @@ const PORT = 3000;
 app.get("/",(req,res)=>{
     res.send("Hello, Abhi here............")
 });
+
+app.get("/quiz",(req,res)=>{
+      res.send(quizzes);
+})
 
 //for heroku process.env.Port needed
 app.listen(process.env.PORT ||PORT,()=>{
