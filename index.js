b12907df26170e26b4ac38a667f34535631f8239
abhirawat623 <abhirawat623@gmail.com>
2023-08-04 14:12:30 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+//to connct BE and FE
+const cors = require("cors");
 //for db
 const quizRouter = require("./router/quiz.router");
 
@@ -7,10 +9,13 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(cors());
+
 app.get("/",(req,res)=>{
     res.send("Hello, Abhi here..123")
 });
 
+//to demonstarate link in main link
 app.use("/quiz",quizRouter);
 
 //for heroku process.env.Port needed
